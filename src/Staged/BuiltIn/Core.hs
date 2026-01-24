e12ef62c242cd03bc36fafe1cd32f355930ab817
Takashi Suwa <bd.gfngfn@gmail.com>
2026-01-24 22:04:00 +0900

    remove unnecessary code

--- src/Staged/BuiltIn/Core.hs
+++ src/Staged/BuiltIn/Core.hs
@@ -168,59 +168,6 @@ unliftBuiltInName = \case
     arity1 = BuiltInArity1
     arity2 = BuiltInArity2
 
-{-
-validateExternalName0 :: Text -> Maybe BuiltIn
-validateExternalName0 = \case
-  "int_add" -> arity2 BIAdd
-  "int_sub" -> arity2 BISub
-  "int_mult" -> arity2 BIMult
-  "int_div" -> arity2 BIDiv
-  "int_mod" -> arity2 BIMod
-  "int_leq" -> arity2 BILeq
-  "int_equal" -> arity2 BIEqual
-  "gen_vadd" -> arity1 BIGenVadd
-  "gen_vconcat" -> arity2 BIGenVconcat
-  "gen_mtranspose" -> arity2 BIGenMtranspose
-  "gen_mconcat_vert" -> arity3 BIGenMconcatVert
-  "drop_at" -> arity2 BIDropAt
-  "broadcastable" -> arity2 BIBroadcastable
-  "broadcast" -> arity2 BIBroadcast
-  "reshapeable" -> arity2 BIReshapeable
-  "list__cons" -> arity2 BIListCons
-  "list__append" -> arity2 BIListAppend
-  "list__iter" -> arity2 BIListIter
-  "tuple__first" -> arity1 BIFst
-  "tuple__second" -> arity1 BISnd
-  "device__gen_cuda_if_available" -> arity1 BIDeviceGenCudaIfAvailable
-  "layer__gen_forward" -> arity2 BILayerGenForward
-  "layer__gen_conv2d_" -> arity8 BILayerGenConv2d
-  "layer__gen_linear" -> arity3 BILayerGenLinear
-  "tensor__gen_zeros" -> arity1 BITensorGenZeros
-  "tensor__gen_add" -> arity2 BITensorGenAdd
-  "tensor__gen_mult" -> arity2 BITensorGenMult
-  "tensor__gen_mm" -> arity3 BITensorGenMm
-  "tensor__gen_grad" -> arity1 BITensorGenGrad
-  "tensor__gen_zero_grad" -> arity1 BITensorGenZeroGrad
-  "tensor__gen_sub_update" -> arity1 BITensorGenSubUpdate
-  "tensor__gen_argmax" -> arity2 BITensorGenArgmax
-  "tensor__gen_cross_entropy_for_logits" -> arity2 BITensorGenCrossEntropyForLogits
-  "tensor__gen_count_equal" -> arity1 BITensorGenCountEqual
-  "tensor__gen_dropout" -> arity1 BITensorGenDropout
-  "tensor__gen_reshape" -> arity2 BITensorGenReshape
-  "tensor__gen_max_pool2d" -> arity7 BITensorGenMaxPool2d
-  "dataset_helper__gen_train_batch" -> arity5 BIDatasetHelperGenTrainBatch
-  "dataset_helper__gen_batch_accuracy" -> arity7 BIDatasetHelperGenBatchAccuracy
-  s -> pure $ BuiltInOther s
-  --  _ -> Nothing
-  where
-    arity1 = pure . BuiltInArity1
-    arity2 = pure . BuiltInArity2
-    arity3 = pure . BuiltInArity3
-    arity5 = pure . BuiltInArity5
-    arity7 = pure . BuiltInArity7
-    arity8 = pure . BuiltInArity8
--}
-
 validateExternalName1 :: Text -> Maybe Ass1BuiltIn
 validateExternalName1 = \case
   "int_add" -> pure A1BIAdd
