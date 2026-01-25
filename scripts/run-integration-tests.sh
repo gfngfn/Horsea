7a61a9033c9283f7bc8884586fafa713ff8a487b
Takashi Suwa <bd.gfngfn@gmail.com>
2026-01-26 05:04:48 +0900

    abolish `integration_tests/success/*` and use `examples/*` directly

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -2,9 +2,41 @@
 
 # Must be run at the root of the repository
 
+TESTS_STAGED_RUN=(
+    examples/gen_vrepeat.lwsd
+    examples/gen_vrepeat_explicit.lwsd
+    examples/mat.lwsd
+    examples/mat_explicit.lwsd
+    examples/mat_concat_vert.lwsd
+    examples/mat_concat_vert_explicit.lwsd
+    examples/polymorphic.lwsd
+    examples/repeat_and_add.lwsd
+    examples/repeat_and_add_explicit.lwsd
+    examples/simple_rec.lwsd
+    examples/tensor_add.lwsd
+    examples/tensor_add_mat.lwsd
+    examples/vec.lwsd
+    examples/vec_explicit.lwsd
+    examples/vec_higher_order.lwsd
+    examples/vec_higher_order_explicit.lwsd
+)
+TESTS_SURFACE_RUN=(
+    examples/gen_vrepeat.surf
+    examples/gen_vrepeat_explicit.surf
+    examples/mat.surf
+    examples/mat_explicit.surf
+    examples/mat_concat_vert.surf
+    examples/mat_concat_vert_explicit.surf
+    examples/repeat_and_add_explicit.surf
+    examples/repeat_and_add.surf
+    examples/simple_rec.surf
+    examples/tensor_add.surf
+    examples/tensor_add_mat.surf
+)
+
 ERRORS=()
 
-for FILE in integration_tests/success/*.lwsd; do
+for FILE in "${TESTS_STAGED_RUN[@]}"; do
     echo "======== $FILE (should pass) ========"
     cabal run horsea -- staged --optimize --distribute-if "$FILE"
     if [ $? -ne 0 ]; then
@@ -26,7 +58,7 @@ for FILE in integration_tests/failure/*.lwsd; do
     fi
 done
 
-for FILE in integration_tests/success/*.surf; do
+for FILE in "${TEST_SURFACE_RUN[@]}"; do
     echo "======== $FILE (should pass) ========"
     cabal run horsea -- surface --optimize --distribute-if "$FILE"
     if [ $? -ne 0 ]; then
