61b553d272f93f2ed089ebcfeb06fba83ef39d82
Takashi Suwa <bd.gfngfn@gmail.com>
2026-02-04 05:34:30 +0900

    remove old scripts

--- scripts/check-staged.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Must be run at the root of the repository
-
-INPUT="$1"
-FILE="examples/ocaml-torch/$INPUT.lwsd"
-echo "======== $FILE ========"
-cabal run horsea -- staged --optimize --distribute-if --compile-time-only "$FILE"
